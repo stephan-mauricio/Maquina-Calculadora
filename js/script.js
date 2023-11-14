9c4634484de1898d1294a28cc7195a6999e52f50
Stephan Maurício <stephanbenevides@gmail.com>
2023-11-14 06:40:17 +0100

    Calculadora
    Desenvolvendo uma calculadora com html, css e js.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function clicar() {
+    let pinput = window.document.getElementsByClassName("pinput")
+
+    let sinput = window.document.getElementsByClassName("sinput") 
+
+    let tinput = window.document.getElementsByClassName("tinput")
+
+}
