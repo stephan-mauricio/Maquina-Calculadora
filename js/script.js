0fa00925f85eeef62a5e5403c6c7e2c0a1f88990
Stephan Maurício <stephanbenevides@gmail.com>
2023-11-14 11:46:26 +0100

    Calculadora X90
    Implementei tags novas ao projeto, aproxima fase será no arquivo de estilo e posteriormente programar a interatividade dos botões com JavaScript.

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function clicar() {
-    let pinput = window.document.getElementsByClassName("pinput")
-
-    let sinput = window.document.getElementsByClassName("sinput") 
-
-    let tinput = window.document.getElementsByClassName("tinput")
-
-}
