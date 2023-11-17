cf4b469269c6d5a584a14aad166144e5bb726a15
Stephan Maurício <stephanbenevides@gmail.com>
2023-11-17 22:17:29 +0100

    Funcionalidades
    Estou programando a interatividade de cada botao.

---
+++ js/script.js
@@ -0,0 +1,129 @@
+function primeirobotao() {
+    let numeroum = document.getElementById("number1").innerText = 1
+    let mostra = window.document.getElementById("mostrar").innerText = numeroum
+}
+
+function segundobotao() {
+    let numerodois = window.document.getElementById("number2").innerText = 2
+    let mostra = window.document.getElementById("mostrar").innerText = numerodois
+}
+
+function terceirobotao() {
+    let numerotres = window.document.getElementById("number3").innerText = 3
+
+    let mostra = window.document.getElementById("mostrar").innerText = numerotres
+}
+
+function quartobotao() {
+    let numeroquatro = window.document.getElementById("number4").innerText = 4
+
+    let mostra = window.document.getElementById("mostrar").innerText = numeroquatro
+}
+
+function quintobotao() {
+    let numerocinco = window.document.getElementById("number5").innerText = 5
+
+    let mostra = window.document.getElementById("mostrar").innerText = numerocinco
+}
+
+function sextobotao() {
+    let numeroseis = window.document.getElementById("number6").innerText = 6
+
+    let mostra = window.document.getElementById("mostrar").innerText = numeroseis
+}
+
+function oitavobotao() {
+    let numerooito = window.document.getElementById("number6").innerText = 6
+
+    let mostra = window.document.getElementById("mostrar").innerText = numeroseis
+}
+
+function setemobotao() {
+    let numerosete = window.document.getElementById("number7").innerText = 7
+
+    let mostra = window.document.getElementById("mostrar").innerText = numerosete
+}
+
+function oitavobotao() {
+    let numerooito = window.document.getElementById("number8").innerText = 8
+
+    let mostra = window.document.getElementById("mostrar").innerText = numerooito
+}
+
+function nonobotao() {
+    let numeronove = window.document.getElementById("number9").innerText = 9
+
+    let mostra = window.document.getElementById("mostrar").innerText = numeronove
+}
+
+function operador_adicao() {
+    let operadoradicao = window.document.getElementById("adicao").innerText = "+"
+    
+    let mostra = window.document.getElementById("mostrar").innerText = operadoradicao
+}
+
+function operador_subtracao() {
+    let operadorsubtracao = window.document.getElementById("subtracao").innerText = "-"
+
+    let mostra = window.document.getElementById("mostrar").innerText = operadorsubtracao
+}
+
+function operador_multiplicacao() {
+    let operadormultiplicacao = window.document.getElementById("multiplicacao").innerText = "x"
+
+    let mostra = window.document.getElementById("mostrar").innerText = operadormultiplicacao
+}
+
+function operador_divisao() {
+    let operadordivisao = window.document.getElementById("divisao").innerText = "/"
+
+    let mostra = window.document.getElementById("mostrar").innerText = operadordivisao
+}
+
+function operador_percentagem() {
+    let operador_percentagem = window.document.getElementById("percentagem").innerText = "%"
+
+    let mostra = window.document.getElementById("mostrar").innerText = operador_percentagem
+}
+
+function operador_raiz() {
+    let operadorraiz = window.document.getElementById("raiz").innerText = "Vx"
+    let mostra = window.document.getElementById("mostrar").innerText = operadorraiz
+}
+
+function calculo() {
+    let numeroum = window.document.getElementById("number1").innerText = 1
+    let numerodois = window.document.getElementById("number2").innerText = 2
+    let numerotres = window.document.getElementById("number3").innerText = 3
+    let numeroquatro = window.document.getElementById("number4").innerText = 4
+    let numerocinco = window.document.getElementById("number5").innerText = 5
+    let numeroseis = window.document.getElementById("number6").innerText = 6
+    let numerosete = window.document.getElementById("number7").innerText = 7
+    let numerooito = window.document.getElementById("number8").innerText = 8
+    let numeronove = window.document.getElementById("number9").innerText = 9    
+    if (numeroum == 1||numerodois == 2) {
+        let mostra = window.document.getElementById("mostrar").innerText = numeroum + numerodois
+    } else if(numerotres == 3 || numeroquatro == 4){
+        mostra = window.document.getElementById("mostrar").innerText = numerotres + numeroquatro
+    }
+}
+
+function comando_apagar() {
+    let mostra = window.document.getElementById("mostrar").innerText = ""
+}
+
+/*
+function resultado() {
+    let number = (
+        [number_1 = window.document.getElementById("number1").innerText = 1], 
+        [number_2 = window.document.getElementById("number2").innerText = 2], 
+        [number_3 = window.document.getElementById("number3").innerText = 3], 
+        [number_4 = window.document.getElementById("number4").innerText = 4], 
+        [number_5 = window.document.getElementById("number5").innerText = 5], 
+        [number_6 = window.document.getElementById("number6").innerText = 6],
+        [number_7 = window.document.getElementById("number7").innerText = 7], 
+        [number_8 = window.document.getElementById("number8").innerText = 8], 
+        [number_9 = window.document.getElementById("number9").innerText = 9]
+    )
+}
+*/
